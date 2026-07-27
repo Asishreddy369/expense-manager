4b63bc389c98bc97a0aaf9fcaade6e708b48f657
Ashish-Vangavaragu <ashish.v@station-s.org>
2026-07-27 15:03:39 +0530

    Demo of the deployment process

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,6 @@ import PersonDetail from './pages/PersonDetail'
 export const ThemeContext = React.createContext()
 
 
-
-
 function App() {
   const [dark, setDark] = useState(() => localStorage.getItem('theme') === 'dark')
 
